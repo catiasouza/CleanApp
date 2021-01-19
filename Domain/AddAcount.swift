57fb4c58a3cd08d01cc2ed6344f985602d1b04d4
Catia <catiamsouza80@gmail.com>
2021-01-19 09:43:34 -0300

    account model

--- Domain/AddAcount.swift
+++ Domain/AddAcount.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 
-protocol AddAcount {
+public protocol AddAcount {
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 }
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passawordConfirmation: String
+public struct AddAccountModel: Encodable {
+    public  var name: String
+    public var email: String
+    public var password: String
+    public var passawordConfirmation: String
+    
+    public init(name: String, email: String, password: String, passawordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passawordConfirmation = passawordConfirmation
+    }
 }
 
