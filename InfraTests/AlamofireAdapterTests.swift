a006e557979ed212c1c6fa1d3271285dd0964022
Catia <catiamsouza80@gmail.com>
2021-01-20 14:50:33 -0300

    remove dados replicados

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -16,27 +16,44 @@ class AlamofireAdapter {
         self.session = session
     }
     func post(to url: URL,with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:Any]
+        
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 class AlamofireAdapterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_amd_method() {
         let url = makeUrl()
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [UrlProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
-        sut.post(to: url, with: makeValidData())
-        let exp = expectation(description: "waiting")
-        UrlProtocolStub.requestObserver { request in
+        testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
+        }
+    }
+    func test_post_should_make_request_with_no_data() {
+        testRequestFor(data: nil) { request in
+            XCTAssertNil(request.httpBodyStream)
+        }
+    }
+}
+extension AlamofireAdapterTests {
+    func makeSut() -> AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        return AlamofireAdapter(session: session)
+    }
+    func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping(URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.post(to: url, with: data)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.requestObserver { request in
+            action(request)
             exp.fulfill()
         }
-       wait(for: [exp], timeout: 1)
+        wait(for: [exp], timeout: 1)
     }
+    
 }
 class UrlProtocolStub: URLProtocol {
     static var emit: ((URLRequest) -> Void)?
@@ -44,10 +61,10 @@ class UrlProtocolStub: URLProtocol {
     static func requestObserver(completion: @escaping(URLRequest) -> Void) {
         UrlProtocolStub.emit = completion
     }
-   override open class func canInit(with request: URLRequest) -> Bool {
+    override open class func canInit(with request: URLRequest) -> Bool {
         return true
     }
-
+    
     override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
@@ -55,8 +72,8 @@ class UrlProtocolStub: URLProtocol {
     override open func startLoading() {
         UrlProtocolStub.emit?( request)
     }
-
+    
     
     override open func stopLoading() { }
-   
+    
 }
